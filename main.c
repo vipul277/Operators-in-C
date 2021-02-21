be74388b8fd0dc73666d8a4cee5364788f0d7476
Vipul Gupta <68422801+vipul277@users.noreply.github.com>
2021-02-21 14:48:40 +0530

    Logical Operators in C and few examples.

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d", 1&&1);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    printf("%d", 1||1);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    printf("%d", !1);
+    return 0;
+}
